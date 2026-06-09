2ff7f76143e5d14aa5f7315803e0fa4c50255201
Samael1269 <sath.mk1010@gmail.com>
2026-06-09 10:10:14 +0800

    src/main/java/com/example/studentmanagementapi/controller/studentcontroller.java git commit -m "Update student controller"

--- src/main/java/com/example/studentmanagementapi/controller/studentcontroller.java
+++ src/main/java/com/example/studentmanagementapi/controller/studentcontroller.java
@@ -1,4 +1,48 @@
 package com.example.studentmanagementapi.controller;
+import com.example.studentapi.model.Student;
+import com.example.studentapi.service.StudentService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/students")
+@CrossOrigin("*")
 
 public class studentcontroller {
+
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @PostMapping
+    public Student addStudent(@RequestBody Student student) {
+        return studentService.addStudent(student);
+    }
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+
+    @GetMapping("/{id}")
+    public Student getStudentById(@PathVariable Long id) {
+        return studentService.getStudentById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Student updateStudent(
+            @PathVariable Long id,
+            @RequestBody Student student
+    ) {
+        return studentService.updateStudent(id, student);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteStudent(@PathVariable Long id) {
+        studentService.deleteStudent(id);
+        return "Student deleted successfully";
+    }
 }
